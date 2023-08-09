95eef003275d8e635c19db9e92caf38ae77748ab
Romeo  Ntazinda <rntazinda@BNRs-MacBook-Pro.local>
2023-08-09 21:03:53 +0200

    paint and sip website home page

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { WebsiteLayout, styled } from 'neza-ui-library'
 import '../styles/globals.css'
 import * as THREE from 'three'
 import { useEffect, useState } from 'react'
+import logo from '../public/logo.png'
 
 const menuItems = [ { label: 'Home', link: '/' }, { label: 'hello', link: '/hello' }, { label: 'FAQ', link: '#faq' }, { label: 'Contact HR', link: '#contact' } ]
 
@@ -11,44 +12,51 @@ const MyApp = ({ Component, pageProps }) => {
 
   const handleToggle = () => setOpened(!opened)
 
-  useEffect(() => {
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000)
+  // useEffect(() => {
+  //   const scene = new THREE.Scene()
+  //   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000)
     
-    const renderer = new THREE.WebGLRenderer({
-      canvas: document.querySelector('#bg')
-    })
+  //   const renderer = new THREE.WebGLRenderer({
+  //     canvas: document.querySelector('#bg')
+  //   })
   
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    camera.position.setZ(30)
+  //   renderer.setPixelRatio(window.devicePixelRatio)
+  //   renderer.setSize(window.innerWidth, window.innerHeight)
+  //   camera.position.setZ(30)
   
 
-    const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-    // const material = new THREE.MeshBasicMaterial({ color: 0x443389, wireframe: true })
-    const material = new THREE.MeshStandardMaterial({ color: 0x443389 })
-    const torus = new THREE.Mesh(geometry, material)
+  //   const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+  //   // const material = new THREE.MeshBasicMaterial({ color: 0x443389, wireframe: true })
+  //   const material = new THREE.MeshStandardMaterial({ color: 0x443389 })
+  //   const torus = new THREE.Mesh(geometry, material)
 
-    const pointLight = new THREE.PointLight(0xffffff)
-    const ambientLight = new THREE.AmbientLight(0xffffff)
-    pointLight.position.set(50,0,50)
+  //   const pointLight = new THREE.PointLight(0xffffff)
+  //   const ambientLight = new THREE.AmbientLight(0xffffff)
+  //   pointLight.position.set(50,0,50)
 
-    scene.add(torus)
-    scene.add(pointLight, ambientLight)
+  //   scene.add(torus)
+  //   scene.add(pointLight, ambientLight)
     
-    renderer.render(scene, camera)
-  }, [])
+  //   renderer.render(scene, camera)
+  // }, [])
 
   return <div>
     <canvas id='bg'></canvas>
     {/* <Menu>
       <MenuItem>Item Home</MenuItem>
     </Menu> */}
-    <ul className={`menu ${opened ? ' opened' : ''}`} >
-      <li>home</li>
-      <li>about</li>
-      <li>hello</li>
-    </ul>
+    <div style={{  textAlign: 'center', paddingTop: '32px' }} >
+      <img src={logo?.src} />
+    </div>
+    <div style={{ display: 'grid', placeContent: 'center', padding: '32px' }} >
+      <ul className={`menu ${opened ? ' opened' : ''}`} style={{ fontSize: '16px', textDecoration: 'none', margin: 'auto' }} >
+        <li style={{ listStyle: 'none' }} >HOME</li>
+        <li style={{ listStyle: 'none' }} >EVENTS</li>
+        <li style={{ listStyle: 'none' }} >BOOKING</li>
+        <li style={{ listStyle: 'none' }} >HOST</li>
+        <li style={{ listStyle: 'none' }} >ABOUT</li>
+      </ul>
+    </div>
     <span className='hamburger' onClick={handleToggle}>
           <span className={`hamburgerIcon ${opened ? ' active' : ''}`} />
           <span className={`hamburgerIcon ${opened ? ' active' : ''}`} />
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,49 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return ( <p>home</p>
+  return ( 
+    <div style={{ margin: 'auto' }} >
+      <img style={{ width: '100%' }} src='https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/hero-new.webp' />
+      <div style={{ display: 'flex', gap: '32px', padding: '32px 64px', placeContent: 'center', maxWidth: '100%', overflowX: 'auto' }} >
+        <div>
+          <img style={{ borderRadius: '50%', width: '96px', height: '96px', objectFit: 'cover', textAlign: 'center' }} src='https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/hero-new.webp' />
+          <p style={{ textAlign: 'center' }} >Women Deliver</p>
+        </div>
+        <div>
+          <img style={{ borderRadius: '50%', width: '96px', height: '96px', objectFit: 'cover', textAlign: 'center' }} src='https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/hero-new.webp' />
+          <p style={{ textAlign: 'center' }}>BK</p>
+        </div>
+        <div>
+          <img style={{ borderRadius: '50%', width: '96px', height: '96px', objectFit: 'cover', textAlign: 'center' }} src='https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/hero-new.webp' />
+          <p style={{ textAlign: 'center' }}>Google</p>
+        </div>
+        <div>
+          <img style={{ borderRadius: '50%', width: '96px', height: '96px', objectFit: 'cover' , textAlign: 'center'}} src='https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/hero-new.webp' />
+          <p style={{ textAlign: 'center' }}>Women Deliver</p>
+        </div>
+        <div>
+          <img style={{ borderRadius: '50%', width: '96px', height: '96px', objectFit: 'cover', textAlign: 'center' }} src='https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/hero-new.webp' />
+          <p style={{ textAlign: 'center' }}>Women Deliver</p>
+        </div>
+      </div>
+      <div style={{ display: 'flex', height: '320px', alignItems: 'center', margin: '32px 64px', width: '960px', margin: 'auto' }} >
+        <h2 style={{ flex: '1' }} >Book a Session</h2>
+        <img src={'https://paintandsipstudios.com.au/storage/2020/06/Finished-Logo-JPG.png'} height='320px' />
+      </div>
+      <div>
+        <h2>Browse through our past events</h2>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '32px', margin: 'auto' }} >
+        {
+          [1, 2, 3, 4, 5, 6, 7, 8, 9].map(a => <div key={a} style={{ padding: '16px', border: '1px solid #333' }} >
+            <img width='240px' src='https://lirp.cdn-website.com/ee739399/dms3rep/multi/opt/group-private-party-cc5eaa0a-640w.jpg' />
+            <div style={{ width: '240px' }} >
+              <h3>Title of the event</h3>
+              <p style={{ color: '#aaa' }} >Some details about the event like date and place, etc.</p>
+            </div>
+          </div>)
+        }
+        </div>
+      </div>
+    </div>
   )
 }
